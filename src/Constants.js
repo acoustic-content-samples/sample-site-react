4220998769a530e200d24696c8137ef4edad9582
Marcin Pasiewicz <marcin.pasiewicz@acoustic.co>
2021-06-22 10:57:06 +0200

    revert unwanted changes

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 export const Constants = {
-	DOMAIN_NAME: 'content-us-1-stage.content-cms.com',
-	SITE_ID: '2dd1b6eb-75b3-47f5-99b3-b27a50369861',
-	CONTENT_HUB_ID: '38cd9790-02d4-4c38-b5ff-7615bdc4243a',
+	//DOMAIN_NAME: 'your-domain-name.com',
+	//SITE_ID: 'default',
+	//CONTENT_HUB_ID: '0000000-0000-0000-0000-000000000000',
 };
